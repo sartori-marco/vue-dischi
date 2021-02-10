59ba260aae5f8ce84ae052a4e538e8ea9d5ecf50
marco sartori <marcosartori98@yahoo.com>
2021-02-10 10:11:57 +0100

    sistemazione select

--- js/main.js
+++ js/main.js
@@ -4,17 +4,20 @@ var app = new Vue({
   el:'#app',
   data: {
     dischi: [],
-    generi:[],
-    prova: ''
+    generi:['All'],
+    genereSelect: 'All'
   },
   mounted(){
     axios
     .get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((result) => {
       this.dischi = result.data.response;
-      // console.log(this.dischi);
+
+
+
+
       this.dischi.forEach((element, i) => {
-        if (!this.generi.includes(element.genre)) {
+        if(!this.generi.includes(element.genre)) {
           this.generi.push(element.genre)
         }
       });
